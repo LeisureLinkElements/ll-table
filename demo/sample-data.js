f2a9eadfabd38a01617d4262cef6986837ac6754
Charles Koehl <ckoehl@leisurelink.com>
2015-10-20 17:57:22 -1000

    OB-400 ll-table: 8 demo tables on one page illustrate all features with various sample data.

--- demo/sample-data.js
+++ demo/sample-data.js
@@ -1,95 +1,145 @@
-window.sampleData = {
-  cols: {
-    assoc: {
-      content: {
-        itemSelector: { // See README for more info.
-          type: 'checkbox'
-        }
-      },
-    },
-    id: {
-      label: 'Item ID' // Not necessary. Just for confirming check-box selections.
-    },
-    first: {
-      label: 'First'
-    },
-    last: {
-      label: 'Last'
-    },
-    phone: {
-      label: 'Phone'
-    },
-    action: {
-      label: 'Action',
-      content: { // Overrides content of each cell in this column.
-        element: {
-          tag: 'span',
-          innerHTML: 'DEL',
-          classes: [
-            'btn',
-            'btn-primary',
-            'remove-unit'
-          ],
-          events: [
-            {
-              in: 'click',
-              out: 'remove-unit',
-              itemIdOnly: true
-            }
-          ]
-        }
-      }
-    }
+var demoItems1 = [
+  {
+    first: 'Darla',
+    last: 'Beaufort',
+    phone: '19997776666'
   },
-  items: [
-    {
-      id: 'c8ske82k',
-      cells: {
-        assoc: true,
-        id:'c8ske82k',
-        first: 'Charles',
-        last: 'Koehl',
-        phone: '18087782244'
+  {
+    first: 'Gordon',
+    last: 'Bolster',
+    phone: '12778737630'
+  },
+  {
+    first: 'Darren',
+    last: 'Levitt',
+    phone: '12129998888'
+  }
+];
+var demoItems2 = [
+  {
+    id: 'c8ske82k',
+    first: 'Darla',
+    last: 'Beaufort',
+    phone: '19997776666'
+  },
+  {
+    id: 'jkwuu833',
+    first: 'Gordon',
+    last: 'Bolster',
+    phone: '12778737630'
+  },
+  {
+    id: 'skjd39ss',
+    first: 'Darren',
+    last: 'Levitt',
+    phone: '12129998888'
+  }
+];
+var demoItems3 = [
+  {
+    id: 'c8ske82k',
+    first: 'Darla',
+    last: 'Beaufort',
+    phone: '19997776666',
+    selected: true
+  },
+  {
+    id: 'jkwuu833',
+    first: 'Gordon',
+    last: 'Bolster',
+    phone: '12778737630',
+    selected: false
+  },
+  {
+    id: 'skjd39ss',
+    first: 'Darren',
+    last: 'Levitt',
+    phone: '12129998888',
+    selected: true
+  }
+];
+var demoItems4 = [
+  {
+    id: 'c8ske82k',
+    first: 'Darla',
+    last: 'Beaufort',
+    phone: '19997776666'
+  },
+  {
+    id: 'jkwuu833',
+    first: 'Gordon',
+    last: {
+      element: {
+        tag: 'span',
+        innerHTML: 'Fun Button',
+        classes: [
+          'btn',
+          'btn-warning'
+        ],
+        events: [
+          {
+            in: 'click',
+            out: 'hey-item-was-clicked'
+          },
+          {
+            in: 'mouseover',
+            out: 'wow-item-moused-over'
+          }
+        ]
       }
     },
-    {
-      id: 'jkwuu833',
-      cells: {
-        assoc: false,
-        id:'jkwuu833',
-        first: 'Gordon',
-        last: {
-          element: {
-          tag: 'span',
-          innerHTML: 'Fun',
-          classes: [
-            'btn',
-            'btn-warning'
-          ],
-          events: [
-            {
-              in: 'click',
-              out: 'clicking-sends-full-data'
-            },
-            {
-              in: 'mouseover',
-              out: 'mouseover-sends-just-item-id',
-              itemIdOnly: true
-            }
-          ]
-        }},
-        phone: '12778737630'
-      }
-    },
-    {
-      id: 'skjd39ss',
-      cells: {
-        assoc: true,
-        id:'skjd39ss',
-        first: 'Darren',
-        last: 'Levitt',
-        phone: '12129998888'
+    phone: '12778737630'
+  },
+  {
+    id: 'skjd39ss',
+    first: 'Darren',
+    last: 'Levitt',
+    phone: '12129998888'
+  }
+];
+
+var demoColumns1 =
+{
+  first: {
+    label: 'First Name'
+  },
+  last: {
+    label: 'Last Name'
+  },
+  phone: {
+    label: 'Telephone'
+  }
+};
+
+var demoColumns2 =
+{
+  first: {
+    label: 'First Name'
+  },
+  last: {
+    label: 'Last Name'
+  },
+  phone: {
+    label: 'Telephone'
+  },
+  action: {
+    label: 'Action',
+    content: { // Overrides content of each cell in this column.
+      element: {
+        tag: 'span',
+        innerHTML: 'DEL',
+        classes: [
+          'btn',
+          'btn-primary',
+          'remove-unit'
+        ],
+        events: [
+          {
+            in: 'click',
+            out: 'delete-it-dude' // Can be any string
+          }
+        ]
       }
     }
-  ]
+  }
 };
