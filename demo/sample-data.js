01dbecb45970e40958199b48aac8e50f39c220e9
Charles Koehl <ckoehl@leisurelink.com>
2015-10-21 10:32:58 -1000

    ll-table: improve demo

--- demo/sample-data.js
+++ demo/sample-data.js
@@ -1,3 +1,7 @@
+/*
+Variable names below to not correspond to demo numbers.
+ */
+
 var demoItems1 = [
   {
     first: 'Darla',
@@ -98,6 +102,39 @@ var demoItems4 = [
   }
 ];
 
+var demoItems5 = [];
+for(var i=0;i<100;i++) {
+  demoItems5.push({
+    id: randomString(20),
+    first: randomString(10),
+    last: randomString(10),
+    phone: randomString(10),
+    addr: randomString(10)
+  });
+}
+
+var demoItems6 = [
+  {
+    id: 'c8ske82k',
+    first_name: 'Darla',
+    last_name: 'Beaufort',
+    phone_num: '19997776666'
+  },
+  {
+    id: 'jkwuu833',
+    first_name: 'Gordon',
+    last_name: 'Bolster',
+    phone_num: '12778737630'
+  },
+  {
+    id: 'skjd39ss',
+    first_name: 'Darren',
+    last_name: 'Levitt',
+    phone_num: '12129998888'
+  }
+];
+
+
 var demoColumns1 =
 {
   first: {
@@ -143,3 +180,7 @@ var demoColumns2 =
     }
   }
 };
+
+function randomString(length) {
+  return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
+}
